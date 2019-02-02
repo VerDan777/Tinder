7a1ca06f7f12850dd47f2a5ce2b0ed3711523aae
Danil Vereschagin <d.vereschagin@wellyes.ru>
2019-02-02 15:01:16 +0300

    Initial Commit

---
+++ Tinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tinder
+//
+//  Created by We//Yes on 02/02/2019.
+//  Copyright © 2019 Daniil Vereschagin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
